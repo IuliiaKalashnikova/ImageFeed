1c2e97688e3b640765820a1f0290c9700d4a552b
Iuliia <julia_kalashnikova01@mail.ru>
2025-02-15 23:08:11 +0100

    Всё сломалось

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,10 @@
+
+import Foundation
+
+enum Constants {
+    static let accessKey = "QyJXQGrLJaQMZhKm91pIAcsikAMmrRKDw3g6STzERmQ"
+    static let secretKey = "qnw8mKfDtuwlG9BrE_sSxXfhiTSpqEz6PlbuZA9OKIs"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL (string: "https://api.unsplash.com")!
+}
