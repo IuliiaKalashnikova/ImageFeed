d4d75af8d8a4a3674cb7bc719cc3fd2360cd6038
Iuliia <julia_kalashnikova01@mail.ru>
2025-02-24 18:48:15 +0300

    Спринт 11

---
+++ ImageFeed/Profile/ProfileImageService.swift
@@ -0,0 +1,71 @@
+
+import Foundation
+struct UserResult: Codable {
+    let profile_image: ProfileImage
+}
+
+ struct ProfileImage: Codable {
+    let small: String
+}
+
+final class ProfileImageService {
+    
+    static let shared = ProfileImageService()
+    static let didChangeNotification = Notification.Name(rawValue: "ProfileImageProviderDidChange")
+    
+    private let urlSession = URLSession.shared
+    private var task: URLSessionTask?
+    
+    private(set) var avatarUrl: String?
+    
+    private init() {}
+    
+    func fetchProfileImageURL(username: String, _ completion: @escaping (Result<String, Error>) -> Void) {
+        
+        assert(Thread.isMainThread)
+        
+        if let task {
+            task.cancel()
+        }
+        
+        guard let request = createURLRequest(username: username) else {
+            return
+        }
+        
+        let task = urlSession.objectTask(for: request) { [weak self] (result: Result<UserResult, Error>) in
+            guard let self else { return }
+            switch result {
+            case .success(let data):
+                let profileImageURL = data.profile_image.small
+                self.avatarUrl = profileImageURL
+                
+                completion(.success(profileImageURL))
+                
+                NotificationCenter.default.post(
+                    name: ProfileImageService.didChangeNotification,
+                    object: self,
+                    userInfo: ["URL": profileImageURL]
+                )
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+        
+        self.task = task
+        task.resume()
+    }
+    
+    private func createURLRequest(username: String) -> URLRequest? {
+        
+         
+        guard let url = URL(string: "https://api.unsplash.com/\(username)") else {
+            print("Ошибка при создании URL")
+            return nil
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("Bearer \(username)", forHTTPHeaderField: "Authorization")
+        return request
+     }
+    
+}
--- ImageFeed/SplashViewController.swift
+++ ImageFeed/SplashViewController.swift
@@ -2,30 +2,60 @@
 import UIKit
 
 final class SplashViewController: UIViewController {
+    
     private let showAuthenticationScreenSegueIdentifier = "ShowAuthenticationScreen"
-
+    
     private let oauth2Service = OAuth2Service.shared
     private let oauth2TokenStorage = OAuth2TokenStorage()
-
+    
+    private let profileService = ProfileService.shared
+    private let storage = OAuth2TokenStorage()
+    
+    private let profileImageService = ImageFeed.ProfileImageService.shared
+    
+    weak var delegate: AuthViewControllerDelegate?
+    
+    private var splashImage: UIImageView = {
+        let image = UIImage(named: "splash_screen_logo")
+        let imageView = UIImageView(image: image)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private func setupUI() {
+        view.addSubview(splashImage)
+        NSLayoutConstraint.activate([
+            splashImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            splashImage.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-
+        
         if oauth2TokenStorage.token != nil {
             switchToTabBarController()
         } else {
-            performSegue(withIdentifier: showAuthenticationScreenSegueIdentifier, sender: nil)
+            let storyboard = UIStoryboard(name: "Main", bundle: .main)
+            let authViewController = storyboard.instantiateViewController(withIdentifier: "AuthViewController") as! AuthViewController
+            
+            authViewController.delegate = self
+            authViewController.modalPresentationStyle = .fullScreen
+            present(authViewController, animated: true, completion: nil)
+            
         }
+        setupUI()
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         setNeedsStatusBarAppearanceUpdate()
     }
-
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         .lightContent
     }
-
+    
     private func switchToTabBarController() {
         guard let window = UIApplication.shared.windows.first else { fatalError("Invalid Configuration") }
         let tabBarController = UIStoryboard(name: "Main", bundle: .main)
@@ -34,19 +64,6 @@ final class SplashViewController: UIViewController {
     }
 }
 
-extension SplashViewController {
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == showAuthenticationScreenSegueIdentifier {
-            guard
-                let navigationController = segue.destination as? UINavigationController,
-                let viewController = navigationController.viewControllers[0] as? AuthViewController
-            else { fatalError("Failed to prepare for \(showAuthenticationScreenSegueIdentifier)") }
-            viewController.delegate = self
-        } else {
-            super.prepare(for: segue, sender: sender)
-        }
-    }
-}
 
 extension SplashViewController: AuthViewControllerDelegate {
     func authViewController(_ vc: AuthViewController, didAuthenticateWithCode code: String) {
@@ -54,8 +71,29 @@ extension SplashViewController: AuthViewControllerDelegate {
             guard let self else { return }
             self.fetchOAuthToken(code)
         }
+        guard let token = storage.token else {
+            return
+        }
+        fetchProfile(token)
     }
-
+    
+    private func fetchProfile(_ token: String) {
+        UIBlockingProgressHUD.show()
+        profileService.fetchProfile(token) { [weak self] result in
+            UIBlockingProgressHUD.dismiss()
+            
+            guard let self = self else { return }
+            
+            switch result {
+            case .success:
+                self.switchToTabBarController()
+                
+            case .failure:
+                break
+            }
+        }
+    }
+    
     private func fetchOAuthToken(_ code: String) {
         oauth2Service.fetchOAuthToken(code) { [weak self] result in
             guard let self = self else { return }
@@ -63,7 +101,6 @@ extension SplashViewController: AuthViewControllerDelegate {
             case .success:
                 self.switchToTabBarController()
             case .failure:
-                // TODO [Sprint 11]
                 break
             }
         }
